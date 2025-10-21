8cee22c44cc4bd8b93638d110b6a5d8dde4f2e1d
Ajay Sharma <ajaysharma44477@gmail.com>
2025-10-21 17:56:38 +0530

    this is my second commit bro

--- lib/BarberBookingApp/detail page.dart
+++ lib/BarberBookingApp/detail page.dart
@@ -19,7 +19,7 @@ class _HomeDetailState extends State<HomeDetail> {
     '12:00 PM', '01:00 PM', '02:00 PM',
     '03:00 PM', '04:00 PM', '05:00 PM',
     '06:00 PM', '07:00 PM', '08:00 PM',
-    '09:00 PM'
+    '09:00 PM','10:00 PM', '11:00 PM'
   ];
   @override
   Widget build(BuildContext context) {
